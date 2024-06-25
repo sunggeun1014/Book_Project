36f4b5782ef04b425e1bc73607b2deef7728bb45
i7 <i7@i7C-44>
2024-06-25 09:35:00 +0900

    DBConnector 생성

---
+++ Pos/src/db/DBConnector.java
@@ -0,0 +1,27 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	
+	private static String driverPath = "oracle.jdbc.driver.OracleDriver";
+	private static String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+	private static String user = "book";
+	private static String password = "1234";
+
+	static {
+		try {
+			Class.forName(driverPath);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
+
+	
+}
